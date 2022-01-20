2d981075b48da17c454d727331863d66a21c964f
Ramadhani Musthofa <ramusthanest@gmail.com>
2022-01-20 09:52:26 +0700

    - update script

--- build-image.sh
+++ build-image.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# build native image
 mvn clean package -Pnative\
   -Dquarkus.container-image.build=true\
   -Dquarkus.container-image.group=registry.heroku.com/rss-engine-native\
   -Dquarkus.container-image.name=web\
   -Dquarkus.container-image.tag=latest
 
+# push to heroku
+docker push registry.heroku.com/rss-engine-native/web
 
-# docker push registry.heroku.com/rss-engine-native/web
-# heroku container:release web --app rss-engine-native
+# trigger release
+heroku container:release web --app rss-engine-native
